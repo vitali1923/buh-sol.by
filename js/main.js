b73e6dd72b8dc3d1d3f09cb959c34478cbfc3fee
vitali1923 <ooomevir@gmail.com>
2022-05-26 09:45:53 +0300

    Добаавил валидацию всех форм

--- js/main.js
+++ js/main.js
@@ -19,4 +19,21 @@ $(document).ready(function (){
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   };
+
+  // Валидация форм
+  $(".form").each(function(){
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+    name: {
+      required: "Введите свое имя",
+      minlength: "Имя не должно быть короче 2 букв"
+    },
+    phone: {
+      required: "Введите номер телефона",
+      minlength: "Не корректный номер телефона"
+    },
+  },
+  });
+  });
 });
